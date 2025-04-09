bb66c01ba93fbd029c12dec4394669655e33f3b2
Awe-olanrewaju <aweolanrewaju@punch.agency>
2025-04-09 12:32:35 +0100

    Revert "Scapper implentation"
    This reverts commit 6a7e4fc0d50e19379172cae8fe197bec4af54291.

--- logging_config.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-def setup_logging(log_file='app.log', level=logging.DEBUG):
-    logging.basicConfig(
-        filename=log_file,
-        level=level,
-        format='%(asctime)s - %(levelname)s - %(message)s'
-    )
